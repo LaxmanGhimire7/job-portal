d836704cde5a5c36a45db687fa2148b54bedf515
Laxman Ghimire <ghimirelaxman2155@gmail.com>
2025-08-16 22:32:14 +0545

    register

--- backend/src/Controller/authController.js
+++ backend/src/Controller/authController.js
@@ -3,10 +3,13 @@ const bcrypt = requier("bcrypt");
 const User = require("../Model/userModel.js");
 
 
-const register = (req, res)=>{
+const register = async(req, res)=>{
     try {
         const {name, email, password, avatar, role} = req.body;
-
+        const userExists = await User.findOne({email});
+        if(userExists){
+            res.status(400).json({status:400, json:"Email already exists"})
+        }
     } catch (error) {
         
     }
